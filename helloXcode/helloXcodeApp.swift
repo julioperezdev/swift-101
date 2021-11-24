424fb7f2d5f5ff607eda3c9e6476f0d103e5bae4
Julio Perez Viloria <protobot.dev@gmail.com>
2021-11-23 23:19:39 -0300

    Initial Commit

---
+++ helloXcode/helloXcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloXcodeApp.swift
+//  helloXcode
+//
+//  Created by Julio Perez Viloria on 23/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct helloXcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
